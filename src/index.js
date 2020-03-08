e569dd124102102dc2e01050da70ec4807d361ff
Paquitosoft <paquitosoftware@gmail.com>
2020-03-08 10:05:45 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const { join } = require('path');
+const Hapi = require('@hapi/hapi');
+
+const VOTES_FILE_PATH = join(__dirname, './votes-status.json');
+
+const votesStatus = require(VOTES_FILE_PATH);
+
+const init = async () => {
+	const server = Hapi.server({
+		port: process.env.PORT || 4444,
+		host: process.env.HOST || 'localhost'
+	});
+	// request.params
+	// request.query
+	// request.payload
+	// handler: (request, helpers) => {}
+
+	server.route({
+		method: 'GET',
+		path: '/api/dj',
+		handler: () => {
+			return { votesStatus };
+		}
+	});
+
+	server.route({
+		method: ['GET', 'POST'],
+		path: '/api/dj/{djId}/vote',
+		handler: (request) => {
+			const djIndex = votesStatus.findIndex(dj => dj.id == request.params.djId);
+			if (djIndex !== -1) {
+				votesStatus[djIndex].votesCount++;				
+			}
+			return { votesStatus };
+		}
+	});
+
+	await server.start();
+	console.log(`Server running on ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', err => {
+	console.log(err);
+	process.exit(1);
+});
+
+init();
